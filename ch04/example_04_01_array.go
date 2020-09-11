d9d592cd3cc525e6e73471c11b017d2a07a38487
gurumee92 <gurumee92@gmail.com>
2020-09-11 21:12:31 +0900

    Update example_04_01_array.go Add 2nd demensions

--- ch04/example_04_01_array.go
+++ ch04/example_04_01_array.go
@@ -27,6 +27,14 @@ func main() {
 	fmt.Println("함수 호출 전", a)
 	SetIndexCallByPoint(&a)
 	fmt.Println("함수 호출 후", a)
+
+	fmt.Println("\n다차원 배열도 만들 수 있어요 다음 처럼 말이죠.")
+	b := [3][3]int{
+		{0, 0, 0},
+		{2: 1},
+		{1},
+	}
+	fmt.Println(b)
 }
 
 func SetIndexCallByValue(a [5]int) {
